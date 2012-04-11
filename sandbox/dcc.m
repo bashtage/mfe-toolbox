b782d52a994da283b7a4f234a157fe96781ee69a
kevin.sheppard <devnull@localhost>
2012-04-11 08:55:07 +0100

    More work on DCC

--- sandbox/dcc.m
+++ sandbox/dcc.m
@@ -1,4 +1,4 @@
-function [parameters,LL,Ht,VCVrobust,likelihoods,scores,diagnostics]=dcc_mvgarch(data,dataAsym,m,l,n,p,o,q,gjrType,type,composite,startingVals,options)
+function [parameters,LL,Ht,VCVrobust,likelihoods,scores,diagnostics]=dcc(data,dataAsym,m,l,n,p,o,q,gjrType,type,composite,startingVals,options)
 
 
 
@@ -12,12 +12,59 @@ function [parameters,LL,Ht,VCVrobust,likelihoods,scores,diagnostics]=dcc_mvgarch
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input Checking
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+switch nargin
+    case 5
+        p = [];
+        o = [];
+        q = [];
+        gjrType = [];
+        type = [];
+        composite = [];
+        startingVals = [];
+        options = [];
+    case 6
+        o = [];
+        q = [];
+        gjrType = [];
+        type = [];
+        composite = [];
+        startingVals = [];
+        options = [];
+    case 7
+        q = [];
+        gjrType = [];
+        type = [];
+        composite = [];
+        startingVals = [];
+        options = [];
+    case 8
+        gjrType = [];
+        type = [];
+        composite = [];
+        startingVals = [];
+        options = [];
+    case 9
+        type = [];
+        composite = [];
+        startingVals = [];
+        options = [];
+    case 10
+        composite = [];
+        startingVals = [];
+        options = [];
+    case 11
+        startingVals = [];
+        options = [];
+    case 12
+        options = [];
+    case 13
+        % Nothing
+    otherwise
+        error('5 to 13 inputs required.')
+end
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Data Transformation
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if ndims(data)==2
+    [T,k] = size(data);
     data2d = data;
     eta = data.*(data<0);
     data = zeros(k,k,T);
@@ -26,12 +73,115 @@ if ndims(data)==2
         data(:,:,t) = data2d(t,:)'*data2d(t,:);
         dataAsym(:,:,t) = eta(t,:)'*eta(t,:);
     end
-else
+elseif ndims(data)==3
+    [k,~,T]=size(data);
     data2d = zeros(K,K,T);
     for i=1:K
         data2d(:,i) = squeeze((1-2*(dataAsym(i,i,:)==0)) .* sqrt(data(i,i,:)));
     end
+else
+    error('DATA must be either a K by T matrix of a K by K by T 3-dimensional array.')
 end
+
+if ~isempty(dataAsym)
+    if ndims(dataAsym)~=3
+        error('DATAASYM must be a K by K by T array.')
+    end
+    [k1,k2,T2] = size(dataAsym);
+    if k1~=k || k2~=k || T2~=T
+        error('DATAASYM must be a K by K by T array.')
+    end
+end
+
+if ~isscalar(m) || floor(m)~=m || m<1
+    error('M must be a positive integer.')
+end
+if isempty(l)
+    l = 0;
+end
+if ~isscalar(l) || floor(l)~=l || l<0
+    error('L must be a non-negative integer.')
+end
+if isempty(n)
+    n = 0;
+end
+if ~isscalar(n) || floor(n)~=n || n<0
+    error('N must be a non-negative integer.')
+end
+
+if isempty(p)
+    p = 1;
+end
+if isscalar(p)
+    p = p * ones(k,1);
+end
+if isempty(o)
+    o = 0;
+end
+if isscalar(o)
+    o = o * ones(k,1);
+end
+if isempty(q)
+    q = 0;
+end
+if isscalar(q)
+    q = q * ones(k,1);
+end
+if any(floor(p)~=p) || any(p<1) || numel(p)~=k
+    error('All elements in P must be positive, and P must be either scalar or K by 1')
+end
+if any(floor(o)~=o) || any(o<1) || numel(o)~=k
+    error('All elements in O must be non-negative, and O must be either scalar or K by 1.')
+end
+if any(floor(q)~=q) || any(q<1) || numel(q)~=k
+    error('All elements in Q must be non-negative, and Q must be either scalar or K by 1.')
+end
+if isempty(gjrType)
+    gjrType = 2;
+end
+if isscalar(gjrType)
+    gjrType = ones(k,1)*gjrType;
+end
+if any(~ismembber(gjrType,[1 2])) || numel(gjrType)~=k
+    error('GJRTYPE must be in {1,2} and mustbe either scalar of K by 1.')
+end
+
+if isempty(type)
+    type = '3-stage';
+end
+type = lower(type);
+if ~ismember(type,{'3-stage','2-stage'})
+    error('TYPE must be either ''3-stage'' or ''2-stage''.')
+end
+if strcmpi(type,'3-stage')
+   type = 3;
+else
+    type = 2;
+end
+
+
+if isempty(composite)
+    composite = 'none';
+end
+composite = lower(composite);
+if ~ismember(composite,{'none','diagonal','full'})
+    error('COMPOSITE must be one of ''None'', ''Diagonal'' or ''Full''.')
+end
+if strcmpi(composite,'none')
+   composite = 0;
+elseif strcmpi(composite,'diagonal')
+    composite = 1;
+else
+    composite = 2;
+end
+if type == 2 && composite==1
+    warning('oxfordMFE:incorrectOption','When TYPE is ''2-stage'', COMPOSITE must be either ''None'' or ''Full''.')
+    composite = 2;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Data Transformation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Univariate volatility models
