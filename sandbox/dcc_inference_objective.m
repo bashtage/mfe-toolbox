f9a3f31f2367e99da4f19e8cf9e7321b2d6690c0
kevin.sheppard <devnull@localhost>
2012-04-13 17:10:03 +0100

    Comments added

--- sandbox/dcc_inference_objective.m
+++ sandbox/dcc_inference_objective.m
@@ -1,5 +1,33 @@
 function [obj,objs] = dcc_inference_objective(parameters,data,dataAsym,m,l,n,univariate) %#ok<INUSL>
-% "Objective" function which has R and N as solution to problem
+% Objective function used by DCC, CCC and related multivariate volatility models to perform
+% inference.  It is not used (and cannot be used) to estimate parameters.
+%
+% USAGE:
+%  [OBJ,OBJS] = dcc_inference_objective(PARAMETERS,DATA,DATAASYM,M,L,N,UNIVARIAT)
+%
+% INPUTS:
+%   PARAMETERS   - Vector of ADCC parameters including possibly volatility and intercepts
+%   DATA         - A T by K matrix of zero mean residuals -OR-
+%                    K by K by T array of covariance estimators (e.g. realized covariance)
+%   DATAASYM     - [OPTIONAL] K by K by T array of asymmetric covariance estimators only needed if
+%                    DATA is 3-dimensional and O>0 or L>0
+%   M            - Order of symmetric innovations in DCC model
+%   L            - Order of asymmetric innovations in ADCC model
+%   N            - Order of lagged correlation in DCC model
+%   UNIVARIATE   - Cell array of structures containing information needed to compute volatilities.
+%
+% OUTPUTS:
+%   OBJ          - "As if" objective for estimating correlation intercept parameters
+%   OBJS         - T by 1 vector of "as if" objectives for estimating correlation intercept parameters
+%
+% COMMENTS:
+%
+% See also DCC, CCC_MVGARCH
+
+% Copyright: Kevin Sheppard
+% kevin.sheppard@economics.ox.ac.uk
+% Revision: 1    Date: 4/13/2012
+
 
 [k,~,T] = size(data);
 % Parse parameters
